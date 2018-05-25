d263fd67f0f3e6e00ac8c1de596a834874e4f3e2
Dmitriy Makarov <xcorp.one@gmail.com>
2018-05-25 03:48:44 +0300

    add SwipePicker

---
+++ swipepicker/src/main/java/one/xcorp/widget/swipepicker/SwipePicker.kt
@@ -0,0 +1,17 @@
+package one.xcorp.widget.swipepicker
+
+import android.content.Context
+import android.text.InputType
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SwipePicker : FrameLayout {
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, null, 0) {
+
+    }
+}
