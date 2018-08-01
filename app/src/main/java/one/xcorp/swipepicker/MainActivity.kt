9fad5d5c9a4ccf012c8bb1c771a884f070205e25
Dmitriy Makarov <xcorp.one@gmail.com>
2018-08-01 21:00:42 +0300

    Added processing for date. Added colorize time. Added time label.

--- app/src/main/java/one/xcorp/swipepicker/MainActivity.kt
+++ app/src/main/java/one/xcorp/swipepicker/MainActivity.kt
@@ -2,17 +2,34 @@ package one.xcorp.swipepicker
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.annotation.ColorInt
 import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import one.xcorp.swipepicker.date.StringInputFilter
 import one.xcorp.swipepicker.time.LocalTime
 import one.xcorp.swipepicker.time.TimeInputFilter
+import one.xcorp.swipepicker.time.TimeOfDay
 import one.xcorp.widget.swipepicker.SwipePicker
+import java.lang.String.valueOf
+import java.util.*
+import java.util.Calendar.DATE
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val DAWN_START = TimeUnit.HOURS.toMillis(5)
+        val DAWN_DURATION = TimeUnit.HOURS.toMillis(4)
+        val SUNSET_START = TimeUnit.HOURS.toMillis(19)
+        val SUNSET_DURATION = TimeUnit.HOURS.toMillis(4)
+    }
+
+    private val colorEvaluator = ColorEvaluator()
     private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,22 +37,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         configureScale()
+        configureDate()
         configureTime()
     }
 
     private fun configureScale() {
-        val negativeBackgroundColor =
-                ContextCompat.getColor(this, R.color.background_negative)
+        val negativeColor = ContextCompat.getColor(this, R.color.negative_background)
 
         scale.setOnValueChangeListener {
-            if (it < 0) {
-                scale.hoverView.colorTint = negativeBackgroundColor
-                scale.setBackgroundInputTint(negativeBackgroundColor)
-            } else {
-                scale.hoverView.colorTint = Color.TRANSPARENT
-                scale.backgroundInputTint = null
-            }
+            scale.setTintColor(if (it < 0) negativeColor else Color.TRANSPARENT)
         }
+    }
+
+    private fun configureDate() {
+        val date = Calendar.getInstance()
+        val stateListener = DateStateChangeListener()
+
+        day.value = (date.get(DATE)).toFloat()
+        day.setOnStateChangeListener(stateListener)
+
+        configureMonth(date, stateListener)
+
+        year.value = (date.get(Calendar.YEAR)).toFloat()
+        year.setOnStateChangeListener(stateListener)
+    }
+
+    private fun configureMonth(date: Calendar, stateListener: SwipePicker.OnStateChangeListener) {
+        val monthsNumbers = (1..12).map(::valueOf).toTypedArray()
+        val monthsValues = resources.getStringArray(R.array.months)
+
+        month.value = (date.get(Calendar.MONTH) + 1).toFloat()
+        // Instead of text InputType, it's better to use number input. In this case done for example.
+        month.addInputFilter(StringInputFilter(*monthsNumbers, *monthsValues))
+        // Convert the value to and from the string
+        month.setValueTransformer(object : SwipePicker.ValueTransformer {
+            override fun stringToFloat(view: SwipePicker, value: String): Float? {
+                val index = monthsValues.indexOfFirst { it.startsWith(value, true) }
+                val number = (index + 1).toFloat()
+                // If string empty or value not found then set month by number or don't change value.
+                val coincidence = number > 0 && !value.isEmpty()
+                return if (coincidence) number else super.stringToFloat(view, value)
+            }
+
+            override fun floatToString(view: SwipePicker, value: Float): String? {
+                return monthsValues[value.toInt() - 1]
+            }
+        })
+        month.setOnValueChangeListener(::setSeason)
+        month.setOnStateChangeListener(stateListener)
     }
 
     private fun configureTime() {
@@ -48,7 +97,7 @@ class MainActivity : AppCompatActivity() {
             override fun stringToFloat(view: SwipePicker, value: String): Float? {
                 val result = LocalTime.parse(completeTime(value), "HH:mm")
                 if (result == null) {
-                    showMessage(R.string.message_incorrect_input)
+                    showMessage(R.string.incorrect_input)
                 }
                 return result?.millis?.toFloat()
             }
@@ -57,6 +106,59 @@ class MainActivity : AppCompatActivity() {
                 return LocalTime(value.toLong()).toString("HH:mm")
             }
         })
+        time.setOnStateChangeListener(object : SwipePicker.OnStateChangeListener {
+            override fun onActivated(view: SwipePicker, isActivated: Boolean) {
+                timeLabel.visibility = if (isActivated) VISIBLE else INVISIBLE
+            }
+        })
+        setTimeOfDay(time.value) // call for init value
+        time.setOnValueChangeListener(::setTimeOfDay)
+    }
+
+    private fun setSeason(value: Float) {
+        val (labelId, colorId) = when (value) {
+            in 9.0f..11.0f -> Pair(R.string.autumn, R.color.autumn_background)
+            in 3.0f..5.0f -> Pair(R.string.spring, R.color.spring_background)
+            in 6.0f..8.0f -> Pair(R.string.summer, R.color.summer_background)
+            else -> Pair(R.string.winter, R.color.winter_background)
+        }
+
+        val color = ContextCompat.getColor(this, colorId)
+
+        monthLabel.setText(labelId)
+        monthLabel.setTextColor(color)
+
+        setDateTintColor(color)
+    }
+
+    private fun setDateTintColor(@ColorInt color: Int) {
+        day.setTintColor(color)
+        month.setTintColor(color)
+        year.setTintColor(color)
+    }
+
+    private fun setTimeOfDay(value: Float) {
+        val selectedTime = LocalTime(value.toLong())
+        val timeOfDay = selectedTime.getTimeOfDay(
+                DAWN_START, DAWN_DURATION, SUNSET_START, SUNSET_DURATION)
+
+        val dayColor = ContextCompat.getColor(this, R.color.day_background)
+        val nightColor = ContextCompat.getColor(this, R.color.night_background)
+
+        val fraction = (value - timeOfDay.start) / timeOfDay.duration()
+        val (color, label) = when (timeOfDay.type) {
+            TimeOfDay.Type.DAWN ->
+                Pair(colorEvaluator.evaluate(fraction, nightColor, dayColor), R.string.dawn)
+            TimeOfDay.Type.DAY -> Pair(dayColor, R.string.day)
+            TimeOfDay.Type.SUNSET ->
+                Pair(colorEvaluator.evaluate(fraction, dayColor, nightColor), R.string.sunset)
+            TimeOfDay.Type.NIGHT -> Pair(nightColor, R.string.night)
+        }
+
+        time.setTintColor(color)
+        timeLabel.setTextColor(color)
+
+        timeLabel.text = getString(label).toLowerCase()
     }
 
     private fun completeTime(value: String): String {
@@ -73,5 +175,22 @@ class MainActivity : AppCompatActivity() {
         toast?.cancel()
         toast = Toast.makeText(this, resId, Toast.LENGTH_LONG)
         toast?.show()
+    }
+
+    private inner class DateStateChangeListener : SwipePicker.OnStateChangeListener {
+
+        override fun onActivated(view: SwipePicker, isActivated: Boolean) {
+            day.isActivated = isActivated
+            month.isActivated = isActivated
+            year.isActivated = isActivated
+
+            if (isActivated) {
+                setSeason(month.value)
+                monthLabel.visibility = VISIBLE
+            } else {
+                setDateTintColor(Color.TRANSPARENT)
+                monthLabel.visibility = INVISIBLE
+            }
+        }
     }
 }
