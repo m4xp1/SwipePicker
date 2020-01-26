63113adca3b31e9b07a2cc5bd2f4a232c0709110
m4xp1 <dm@xcorp.one>
2020-01-26 04:59:57 +0300

    Refactor HoverView

--- swipepicker/src/main/java/one/xcorp/widget/swipepicker/HoverView.kt
+++ swipepicker/src/main/java/one/xcorp/widget/swipepicker/HoverView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.support.annotation.ColorInt
 import android.support.v4.content.ContextCompat
-import android.support.v4.view.ViewCompat
+import android.support.v4.view.ViewCompat.getMinimumWidth
 import android.util.AttributeSet
 import android.view.View
 
@@ -17,19 +17,19 @@ class HoverView : View {
             paint.textSize = value
             requestLayout()
         }
-    var textColor: Int = 0
+    var textColor: Int
         @ColorInt get
         set(@ColorInt value) {
             field = value
             invalidate()
         }
-    var color: Int = 0
+    var color: Int
         @ColorInt get
         set(@ColorInt value) {
             field = value
             invalidate()
         }
-    var colorTint: Int = 0
+    var colorTint: Int
         @ColorInt get
         set(@ColorInt value) {
             field = value
@@ -48,7 +48,7 @@ class HoverView : View {
     private val backgroundRect = RectF()
     private val arcRect = RectF()
     private val backgroundPath = Path()
-    private var radiusCorner = 0f
+    private var radiusCorner: Float
 
     private val digits = Regex("\\d")
     private val wideDigit = "4"
@@ -60,52 +60,49 @@ class HoverView : View {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
             this(context, attrs, defStyleAttr, R.style.XcoRp_Style_SwipePicker_HoverView)
 
-    constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
-                defStyleRes: Int = R.style.XcoRp_Style_SwipePicker_HoverView)
-            : super(context, attrs, defStyleAttr) {
+    constructor(
+            context: Context,
+            attrs: AttributeSet? = null,
+            defStyleAttr: Int = 0,
+            defStyleRes: Int = R.style.XcoRp_Style_SwipePicker_HoverView
+    ) : super(context, attrs, defStyleAttr) {
+        val typedArray = context.obtainStyledAttributes(
+                attrs, R.styleable.HoverView, defStyleAttr, defStyleRes)
+        minimumWidth = typedArray.getDimensionPixelSize(R.styleable.HoverView_android_minWidth,
+                resources.getDimensionPixelSize(R.dimen.hoverView_minWidth))
+        val padding = typedArray.getDimensionPixelSize(R.styleable.HoverView_android_padding,
+                resources.getDimensionPixelSize(R.dimen.hoverView_padding))
+        setPadding(padding, padding, padding, padding)
+        textSize = typedArray.getDimensionPixelSize(R.styleable.HoverView_android_textSize,
+                resources.getDimensionPixelSize(R.dimen.hoverView_textSize)).toFloat()
+        textColor = typedArray.getColor(R.styleable.HoverView_android_textColor,
+                ContextCompat.getColor(context, R.color.swipePicker_textInverse))
+        color = typedArray.getColor(R.styleable.HoverView_android_color,
+                ContextCompat.getColor(context, R.color.swipePicker_primary))
+        colorTint = typedArray.getColor(R.styleable.HoverView_colorTint, Color.TRANSPARENT)
+        typedArray.recycle()
+
         paint.apply {
             isAntiAlias = true
             textAlign = Paint.Align.CENTER
         }
 
-        with(context.obtainStyledAttributes(attrs, R.styleable.HoverView, defStyleAttr, defStyleRes)) {
-            try {
-                minimumWidth = getDimensionPixelSize(R.styleable.HoverView_android_minWidth,
-                        resources.getDimensionPixelSize(R.dimen.hoverView_minWidth))
-                val padding = getDimensionPixelSize(R.styleable.HoverView_android_padding,
-                        resources.getDimensionPixelSize(R.dimen.hoverView_padding))
-                setPadding(padding, padding, padding, padding)
-                textSize = getDimensionPixelSize(R.styleable.HoverView_android_textSize,
-                        resources.getDimensionPixelSize(R.dimen.hoverView_textSize)).toFloat()
-                textColor = getColor(R.styleable.HoverView_android_textColor,
-                        ContextCompat.getColor(context, R.color.swipePicker_textInverse))
-                color = getColor(R.styleable.HoverView_android_color,
-                        ContextCompat.getColor(context, R.color.swipePicker_primary))
-                colorTint = getColor(R.styleable.HoverView_colorTint, Color.TRANSPARENT)
-
-                radiusCorner = resources.getDimensionPixelSize(R.dimen.hoverView_radiusCorner).toFloat()
-
-            } finally {
-                recycle()
-            }
-        }
+        radiusCorner = resources.getDimensionPixelSize(R.dimen.hoverView_radiusCorner).toFloat()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val ems = text.toString().replace(digits, wideDigit)
         paint.getTextBounds(ems, 0, ems.length, contentRect)
 
-        val right = Math.max(ViewCompat.getMinimumWidth(this),
-                paddingLeft + contentRect.width() + paddingRight)
+        val right = getMinimumWidth(this)
+                .coerceAtLeast(paddingLeft + contentRect.width() + paddingRight)
         val bottom = paddingTop + textSize.toInt() + paddingBottom
         contentRect.set(0, 0, right, bottom)
 
         val arrowSize = contentRect.height() / 2.5f
 
-        backgroundRect.apply {
-            set(contentRect)
-            this.bottom += arrowSize
-        }
+        backgroundRect.set(contentRect)
+        backgroundRect.bottom += arrowSize
 
         defineBackgroundPath(backgroundRect, radiusCorner, arrowSize)
         setMeasuredDimension(backgroundRect.width().toInt(), backgroundRect.height().toInt())
